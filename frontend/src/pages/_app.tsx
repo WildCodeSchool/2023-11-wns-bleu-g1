0c3c624434d97003d7ab14bfd4000496450a8c83
Ghost <vincent.aipar@gmail.com>
2024-04-25 16:47:35 +0400

    change apollo client url

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import '@/styles/globals.css';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import type { AppProps } from 'next/app';
+import "@/styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import type { AppProps } from "next/app";
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql',
+	uri: "http://localhost:4000/",
 	cache: new InMemoryCache({
 		addTypename: false,
 	}),
