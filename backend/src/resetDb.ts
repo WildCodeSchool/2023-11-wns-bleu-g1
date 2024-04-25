303a89c8ac8dd771e0f1f6312ecc3b9b7456627b
Thomas <thomasmarchand63@gmail.com>
2024-04-25 10:25:21 +0200

    :sparkles: add reset DB

---
+++ backend/src/resetDb.ts
@@ -0,0 +1,33 @@
+import db from "./db";
+import User from "./entities/user";
+
+const cleanDb = async () => {
+  const runner = db.createQueryRunner();
+  await runner.query("SET session_replication_role = 'replica'");
+  await Promise.all(
+    db.entityMetadatas.map((entity) =>
+      runner.query(`ALTER TABLE "${entity.tableName}" DISABLE TRIGGER ALL`)
+    )
+  );
+
+  await Promise.all(
+    db.entityMetadatas.map((entity) =>
+      runner.query(`DROP TABLE IF EXISTS "${entity.tableName}" CASCADE`)
+    )
+  );
+  await runner.query("SET session_replication_role = 'origin'");
+  await db.synchronize();
+};
+
+const main = async () => {
+  await db.initialize();
+
+  await cleanDb();
+
+  const user = await User.create({ email: "test@test.test" }).save();
+
+  await db.destroy();
+  console.log(`${user.email} created`);
+};
+
+main();
