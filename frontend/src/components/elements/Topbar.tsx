8e1497da52ab9ad63c687dbffede31458eb193c8
Pouish85 <pouish.iphone@gmail.com>
2024-04-30 18:30:33 +0200

    feat: mobile landing page

--- frontend/src/components/elements/Topbar.tsx
+++ frontend/src/components/elements/Topbar.tsx
@@ -4,13 +4,17 @@ import { Button } from "@/components/ui/button";
 
 type TopbarProps = {
 	isTopOfPage: boolean;
-}
+};
 
-const Topbar = ({isTopOfPage}: TopbarProps) => {
-	const topbarBackground = isTopOfPage ? "bg-topbarbackground/[33%]" : "bg-topbarbackground";
+const Topbar = ({ isTopOfPage }: TopbarProps) => {
+	const topbarBackground = isTopOfPage
+		? "bg-topbarbackground/[33%]"
+		: "bg-topbarbackground";
 
 	return (
-		<div className={`${topbarBackground} fixed top-0 flex h-[10%] w-full items-center justify-between px-2 align-middle transition ease-in-out duration-700`}>
+		<div
+			className={`${topbarBackground} fixed top-0 flex h-[10%] w-full items-center justify-between px-2 align-middle transition duration-700 ease-in-out`}
+		>
 			<Image src="logo.svg" alt="Logo" width={100} height={100} />
 			<Button>Se connecter</Button>
 		</div>
