8366f940b7083d90edfdd3c75f2b491a4c23747d
Pouish85 <pouish.iphone@gmail.com>
2024-05-01 20:09:25 +0200

    feat: colorize desktop terminal

--- frontend/src/components/ui/textarea.tsx
+++ frontend/src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
 		return (
 			<textarea
 				className={cn(
-					"text-sm text-black dark:text-white bg-lightterminalbackground dark:bg-darkterminalbackground ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:mt-4 rounded-md border-none min-h-80 md:min-h-[50vh] md:min-w-[45%] p-2 resize-none m-1",
+					"text-[15pt] resize-none w-[calc(100%-32px)] h-[500px] font-mono border-none m-2.5 p-2 absolute top-0 bg-lightterminalbackground dark:bg-darkterminalbackground rounded-md md:mt-4 leading-[20pt]",
 					className
 				)}
 				ref={ref}
@@ -20,5 +20,5 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
 	}
 );
 Textarea.displayName = "Textarea";
-
+// text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:mt-4 rounded-md border-none p-2 resize-none m-1 w-[90%] h-full font-mono
 export { Textarea };
--- frontend/src/pages/coding/codingPage.tsx
+++ frontend/src/pages/coding/codingPage.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 // import Topbar from "@/components/elements/Topbar";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import PrismLoader from "@/components/prism-loader";
+import Prism from "prismjs";
 
 function CodingPage() {
 	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+	const [input, setInput] = useState("");
 	useEffect(() => {
 		const handleScroll = () => {
 			if (window.scrollY === 0) {
@@ -18,12 +21,16 @@ function CodingPage() {
 		window.addEventListener("scroll", handleScroll);
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, []);
-	const [textArea, setTextArea] = useState("");
-
-	const handleSumbit = (e) => {
-		e.preventDefault();
-		//  console here form data
-	};
+	console.log(input);
+	function update(text) {
+		let result_element = document.querySelector("#highlighting-content");
+		// Update code
+		result_element.innerHTML = text
+			.replace(new RegExp("&", "g"), "&")
+			.replace(new RegExp("<", "g"), "<");
+		// Syntax Highlight
+		Prism.highlightElement(result_element);
+	}
 
 	return (
 		<main className="min-h-[100vh]">
@@ -44,10 +51,29 @@ function CodingPage() {
 					</div>
 				</div>
 				<div id="codingContent" className="flex flex-col md:flex-row w-full">
-					<Textarea
-						className="md:ml-4"
-						placeholder="Commencez a coder ici..."
-					/>
+					<div
+						id="codingArea"
+						className="relative min-h-80 md:min-h-[50vh] md:min-w-[45%]"
+					>
+						<Textarea
+							className="left-0 z-10 caret-white md:ml-4 bg-transparent text-transparent leading-[20pt] text-[15pt]"
+							placeholder="Commencez a coder ici..."
+							id="codingInput"
+							onChange={(e) => update(e.target.value)}
+							spellCheck="false"
+						/>
+						<pre
+							className="left-0 z-0 md:ml-4 text-[15pt] resize-none w-[calc(100%-32px)] h-[500px] font-mono border-none m-2.5 p-2 absolute top-0 bg-lightterminalbackground dark:bg-darkterminalbackground rounded-md md:mt-4 leading-[20pt]"
+							id="highlightedCodingContent"
+							aria-hidden="true"
+						>
+							<code
+								className="language-js text-[15pt] leading-[20pt] font-mono"
+								id="highlighting-content"
+							></code>
+						</pre>
+						<PrismLoader />
+					</div>
 					<div className="flex flex-row-reverse md:flex-col w-full justify-center text-center align-center md:items-center">
 						<Button
 							size={"sm"}
@@ -60,9 +86,21 @@ function CodingPage() {
 							Pour ne plus avoir de limites, passer premium!
 						</p>
 					</div>
-					<Textarea className="md:mr-4" />
+					<div
+						id="resultArea"
+						className="relative min-h-80 md:min-h-[50vh] md:min-w-[45%]"
+					>
+						<Textarea readOnly={true} className="left-0 md:mr-4" />
+					</div>
 				</div>
 			</div>
+			{/*<div>*/}
+			{/*	<Textarea id="editing" onChange={(e) => update(e.target.value)} spellCheck="false"/>*/}
+			{/*	<pre className="language-javascript" id="highlighting" aria-hidden="true">*/}
+			{/*		<code className="language-js" id="highlighting-content"></code>*/}
+			{/*	</pre>*/}
+			{/*	<PrismLoader />*/}
+			{/*</div>*/}
 		</main>
 	);
 }
