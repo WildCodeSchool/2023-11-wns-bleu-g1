85e50ba338abceeb6a50d2cf7fa871cc4a3a0e7b
Pouish85 <pouish.iphone@gmail.com>
2024-05-01 10:34:05 +0200

    feat: desktop codding page skeloton

--- frontend/src/pages/coding/codingPage.tsx
+++ frontend/src/pages/coding/codingPage.tsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 // import Topbar from "@/components/elements/Topbar";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 function CodingPage() {
-    const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 	useEffect(() => {
 		const handleScroll = () => {
 			if (window.scrollY === 0) {
@@ -17,14 +17,15 @@ function CodingPage() {
 		window.addEventListener("scroll", handleScroll);
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, []);
-    return (
-        <main>
-            {/*<Topbar isTopOfPage={isTopOfPage} />*/}
-            <div className="">
+	return (
+		<main className="min-h-[100vh]">
+			{/*<Topbar isTopOfPage={isTopOfPage} />*/}
+			<div className="">
 				<div id="coddingTopInfo" className="flex w-full relative border-b">
-					<h1 className="flex flex-1 justify-start align-middle items-center pl-4">Nom du projet</h1>
-					<div
-						className="relative my-6 flex h-10 w-12 rounded-md md:h-14 justify-end align-bottom items-end">
+					<h1 className="flex flex-1 justify-start align-middle items-center pl-4">
+						Nom du projet
+					</h1>
+					<div className="relative my-6 mr-4 flex h-10 w-12 rounded-md md:h-14 justify-end align-bottom items-end">
 						<Image
 							src="/Javascript_logo.png"
 							alt="logo javascript"
@@ -34,16 +35,21 @@ function CodingPage() {
 						/>
 					</div>
 				</div>
-				<div id="codingContent" className="flex flex-col w-full">
-					<textarea className="border-none min-h-80 p-2 resize-none m-1" placeholder="Commencez a coder ici..."></textarea>
+				<div id="codingContent" className="flex flex-col md:flex-row w-full">
+					<textarea
+						className="border-none min-h-80 md:min-h-[50vh] md:min-w-[45%] p-2 resize-none m-1 md:ml-4 md:mt-4 rounded-md"
+						placeholder="Commencez a coder ici..."
+					></textarea>
 					<div className="flex w-full justify-end">
-						<Button size={"sm"} className="flex mt-4 mb-4 w-20 mr-4">Exécuter</Button>
+						<Button size={"sm"} className="flex mt-4 mb-4 w-20 mr-4">
+							Exécuter
+						</Button>
 					</div>
-					<textarea className="border-none min-h-80 p-2 resize-none m-1" ></textarea>
+					<textarea className="border-none min-h-80 md:min-h-[50vh] md:min-w-[45%] p-2 resize-none m-1 md:mr-4 md:mt-4 rounded-md"></textarea>
 				</div>
 			</div>
 		</main>
-	)
+	);
 }
 
-export default CodingPage
+export default CodingPage;
