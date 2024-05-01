b736afd05b90a28766465aaf746e013c24d09fb3
Pouish85 <pouish.iphone@gmail.com>
2024-05-01 14:52:30 +0200

    feat: use shadcn component textarea

--- frontend/src/pages/coding/codingPage.tsx
+++ frontend/src/pages/coding/codingPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 // import Topbar from "@/components/elements/Topbar";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea"
+import { Textarea } from "@/components/ui/textarea";
 
 function CodingPage() {
 	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
@@ -18,13 +18,12 @@ function CodingPage() {
 		window.addEventListener("scroll", handleScroll);
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, []);
-	const [textArea, setTextArea] = useState('')
+	const [textArea, setTextArea] = useState("");
 
-	const handleSumbit = (e) =>{
+	const handleSumbit = (e) => {
 		e.preventDefault();
-	   //  console here form data
-	}
-
+		//  console here form data
+	};
 
 	return (
 		<main className="min-h-[100vh]">
@@ -34,8 +33,7 @@ function CodingPage() {
 					<h1 className="flex flex-1 justify-start align-middle items-center pl-4">
 						Nom du projet
 					</h1>
-					<div
-						className="relative my-6 mr-4 flex h-10 w-12 rounded-md md:h-14 justify-end align-bottom items-end">
+					<div className="relative my-6 mr-4 flex h-10 w-12 rounded-md md:h-14 justify-end align-bottom items-end">
 						<Image
 							src="/Javascript_logo.png"
 							alt="logo javascript"
@@ -51,15 +49,18 @@ function CodingPage() {
 						placeholder="Commencez a coder ici..."
 					/>
 					<div className="flex flex-row-reverse md:flex-col w-full justify-center text-center align-center md:items-center">
-						<Button size={"sm"} className="flex md:justify-center md:items-center md:content-center md:align-middle mt-4 mb-4 w-20 ml-2 md:mr-0">
+						<Button
+							size={"sm"}
+							className="flex md:justify-center md:items-center md:content-center md:align-middle mt-4 mb-4 w-20 ml-2 md:mr-0"
+						>
 							Exécuter
 						</Button>
 						<p className="flex items-center">0/50</p>
-						<p className="flex items-center">Pour ne plus avoir de limites, passer premium!</p>
+						<p className="flex items-center">
+							Pour ne plus avoir de limites, passer premium!
+						</p>
 					</div>
-					<Textarea
-						className="md:mr-4"
-					/>
+					<Textarea className="md:mr-4" />
 				</div>
 			</div>
 		</main>
