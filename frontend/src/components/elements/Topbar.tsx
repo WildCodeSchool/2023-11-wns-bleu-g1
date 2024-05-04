1ec5fcac25119488fa57c56f580a39b95e8e806a
Pouish85 <pouish.iphone@gmail.com>
2024-05-04 09:22:45 +0200

    feat: use Logo as component and use it in topbar

--- frontend/src/components/elements/Topbar.tsx
+++ frontend/src/components/elements/Topbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Logo from "@/components/elements/Logo";
 
 type TopbarProps = {
 	isTopOfPage: boolean;
@@ -17,7 +18,7 @@ const Topbar = ({ isTopOfPage }: TopbarProps) => {
 			className={`${topbarBackground} fixed top-0 h-fit w-full flex items-center py-4 z-50`}
 		>
 			<div className="container mx-auto flex items-center justify-between align-middle transition duration-700 ease-in-out">
-				<Image src="logo.svg" alt="Logo" width={75} height={75} />
+				<Logo />
 				<Link href={"/auth/connexion"} className="text-primary hover:underline">
 					<Button border={border} className={`${border}`}>
 						Se connecter
