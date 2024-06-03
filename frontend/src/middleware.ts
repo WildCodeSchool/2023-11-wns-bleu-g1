ad47a6508c6fb3354fcf5072d23bbbbfe6fadcfb
Ghost <vincent.aipar@gmail.com>
2024-06-03 18:46:53 +0400

    add token check on auth routes

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -9,6 +9,20 @@ const JWT_PRIVATE_KEY = new TextEncoder().encode(
 export async function middleware(request: NextRequest) {
 	const token = request.cookies.get("token")?.value;
 
+	if (request.nextUrl.pathname.startsWith("/auth")) {
+		if (token) {
+			try {
+				const { payload } = await jwtVerify(token, JWT_PRIVATE_KEY);
+				if (payload.userId)
+					return NextResponse.redirect(
+						new URL("/tableau-de-bord", request.url)
+					);
+			} catch (e) {}
+		} else {
+			return NextResponse.next();
+		}
+	}
+
 	if (token) {
 		try {
 			const { payload } = await jwtVerify(token, JWT_PRIVATE_KEY);
@@ -21,5 +35,5 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-	matcher: ["/tableau-de-bord"],
+	matcher: ["/tableau-de-bord", "/auth/:path*"],
 };
