97b7383c4093a8816563c0059a916a161bcee076
Pouish85 <pouish.iphone@gmail.com>
2024-06-05 11:20:54 +0200

    refactor: reroll backend files

--- backend/src/resolvers/user.resolver.ts
+++ backend/src/resolvers/user.resolver.ts
@@ -16,44 +16,58 @@ export default class UserResolver {
 		const users = await User.find();
 		return users;
 	}
+
 	@Mutation(() => User)
 	async createUser(@Arg("data", { validate: true }) data: NewUserInput) {
 		if (!data.email) {
 			throw new GraphQLError("email is missing but require");
 		}
+
 		if (!data.pseudo) {
 			throw new GraphQLError("pseudo is missing but require");
 		}
+
 		if (!data.password) {
 			throw new GraphQLError("password is missing but require");
 		}
+
 		// SELECT * FROM User WHERE email=data.email
 		const userAlreadyExist = await User.findOneBy({ email: data.email });
 		// SELECT * FROM User WHERE pseudo=data.pseudo
 		const pseudoAlreadyExist = await User.findOneBy({
 			pseudo: data.pseudo.toLocaleLowerCase(),
 		});
+
 		if (userAlreadyExist) {
 			throw new GraphQLError(`user: ${data.email} already exist`);
 		}
+
 		if (pseudoAlreadyExist) {
 			throw new GraphQLError(`pseudo: ${data.pseudo} is already taken`);
 		}
+
 		const newUser = new User();
+
 		Object.assign(newUser, data);
+
 		return await newUser.save();
 	}
+
 	@Mutation(() => String)
 	async signin(@Arg("data") data: SigninInput, @Ctx() ctx: Context) {
 		// SELECT * FROM User WHERE email=data.email
 		const user = await User.findOneBy({ email: data.email });
+
 		if (!user) {
 			throw new GraphQLError(`email: ${data.email} not register`);
 		}
+
 		const isUserPassword = await verify(user.hashedPassword, data.password);
+
 		if (!isUserPassword) {
 			throw new GraphQLError("invalid password");
 		}
+
 		const token = jwt.sign(
 			{
 				userId: user.id,
@@ -61,16 +75,20 @@ export default class UserResolver {
 			env.JWT_PRIVATE_KEY,
 			{ expiresIn: "30d" }
 		);
+
 		ctx.res.cookie("token", token, {
 			httpOnly: true,
 			maxAge: 30 * 24 * 60 * 60 * 1000,
 			secure: env.NODE_ENV === "production",
 		});
+
 		return token;
 	}
+
 	@Mutation(() => String)
 	async logout(@Ctx() ctx: Context) {
 		ctx.res.clearCookie("token");
+
 		return "ok";
 	}
 }
