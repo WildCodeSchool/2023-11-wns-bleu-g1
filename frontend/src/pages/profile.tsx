9135b6552e310a3a3c404740f4db4df9b9c14231
Alexandre HAMM <a.hamm@codeur.online>
2024-06-05 16:20:54 +0200

    feature/public_profile_page

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -4,13 +4,13 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Profile() {
 	return (
 		<main
-			className={`flex min-h-screen flex-col items-center justify-between pt-24 px-16 ${inter.className}`}
+			className={`flex min-h-screen flex-col items-center justify-between py-24 px-16 ${inter.className}`}
 			>
 				<header
-					className={`flex gap-5`}>
+					className={`flex gap-5 w-full max-w-3xl items-center`}>
 						<div
 							className={`overflow-hidden rounded-full border`}>
 								<Image
@@ -22,19 +22,136 @@ export default function Home() {
 									/>
 						</div>
 						<div
-							className={`max-w-96 text-ellipsis overflow-hidden flex flex-col justify-between`}>
+							className={`max-w-[60%] text-ellipsis overflow-hidden flex flex-col gap-2`}>
 								<h3
-									className={`text-4xl`}>
-										{"user exists" == true ? "nom de l'utilisateur" : "zNeossssssssssssssss"}
+									className={`text-4xl text-center`}>
+										{"user exists" == true ? "nom de l'utilisateur" : "Utilisateur introuvable"}
 								</h3>
 								{/* BADGE == COMPOSANT {{ A FAIRE }} */}
 								<p
-									className={`text-center rounded-full bg-destructive`}
+									className={`text-center rounded-full bg-destructive text-xs py-1`}
 									title={"user exists" == true ? "role de l'utilisateur" : "Flex Master"}>
 										{"user exists" == true ? "role de l'utilisateur" : "Flex Master"}
 								</p>
 						</div>
 				</header>
+
+				<section 
+					className={`flex flex-col gap-5 w-full max-w-3xl text-xl`}
+					>
+
+					<div
+						className={`flex flex-col gap-6 border-b-2 border-foreground pb-8`}
+						>
+						<p
+							className={`flex gap-5 items-center`}
+							>
+							<span
+								className={`flex justify-center items-center rounded-full bg-foreground text-background text-white min-w-[50px] min-h-[50px]`}
+								>
+								58
+							</span> 
+							Projets
+						</p>
+						<p
+							className={`flex gap-5 items-center`}
+							>
+							<span
+								className={`flex justify-center items-center rounded-full bg-foreground text-background text-white min-w-[50px] min-h-[50px]`}
+								>
+								125
+							</span> 
+							Likes
+						</p>
+						<p
+							className={`flex gap-5 items-center`}
+							>
+							<span
+								className={`flex justify-center items-center rounded-full bg-foreground text-background text-white min-w-[50px] min-h-[50px]`}
+								>
+								58
+							</span> 
+							Commentaires
+						</p>
+						
+					</div>
+
+					<div
+						className={`flex flex-col gap-4 min-h-[150px]`}
+						>
+						<h3 className={`text-center mb-4`}
+							>Bests projects</h3>
+
+						<div
+							className={`flex gap-5 text-base`}
+							>
+								<p
+									className={`flex items-center bg-foreground text-background min-w-[65%] px-3 rounded-xl`}
+								>
+									Hack the NASA
+								</p>
+								
+								<span
+									className={`flex items-center gap-2`}
+									>
+									<strong className={`text-2xl`}>★</strong>
+									25
+								</span>
+						</div>
+
+						<div
+							className={`flex gap-5 text-base`}
+							>
+								<p
+									className={`flex items-center bg-foreground text-background min-w-[65%] px-3 rounded-xl`}
+								>
+									Elon is afraid
+								</p>
+								
+								<span
+									className={`flex items-center gap-2`}
+									>
+									<strong className={`text-2xl`}>★</strong>
+									25
+								</span>
+						</div>
+
+						<div
+							className={`flex gap-5 text-base`}
+							>
+								<p
+									className={`flex items-center bg-foreground text-background min-w-[65%] px-3 rounded-xl`}
+								>
+									Youtube unlimited
+								</p>
+								
+								<span
+									className={`flex items-center gap-2`}
+									>
+									<strong className={`text-2xl`}>★</strong>
+									25
+								</span>
+						</div>
+
+						<div
+							className={`flex gap-5 text-base`}
+							>
+								<p
+									className={`flex items-center bg-foreground text-background min-w-[65%] px-3 rounded-xl`}
+								>
+									REACT Marios Bros.
+								</p>
+								
+								<span
+									className={`flex items-center gap-2`}
+									>
+									<strong className={`text-2xl`}>★</strong>
+									25
+								</span>
+						</div>
+					</div>
+
+				</section>
 		</main>
 	);
 }
