f8f66048362c09cd6f2c67495a8b09a31672616c
Pouish85 <pouish.iphone@gmail.com>
2024-06-05 11:15:53 +0200

    refactor: reroll backend files

---
+++ backend/__tests__/helpers/getAdminJwt.ts
@@ -0,0 +1,22 @@
+import User, { UserRole } from "../../src/entities/user";
+import jwt from "jsonwebtoken";
+import env from "../../src/env";
+
+export default async function getAdminJwt() {
+	const admin = new User();
+
+	Object.assign(admin, {
+		role: UserRole.ADMIN,
+		email: "ad@min.fr",
+		pseudo: "i'm admin",
+		password: "Test123456!",
+	});
+
+	await admin.save();
+
+	const JWT = await jwt.sign({ userId: admin.id }, env.JWT_PRIVATE_KEY);
+
+	console.log(JWT);
+
+	return { admin, JWT };
+}
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 import env from "./env";
 import User from "./entities/user";
 
-export default new DataSource({
+const db = new DataSource({
 	type: "postgres",
 	password: env.DB_PASS,
 	username: env.DB_USER,
@@ -11,4 +11,15 @@ export default new DataSource({
 	port: env.DB_PORT,
 	entities: [User],
 	synchronize: true,
+	logging: env.NODE_ENV !== "test",
 });
+
+export async function clearDb() {
+	const entities = db.entityMetadatas;
+	const tableNames = entities
+		.map((entity) => `"${entity.tableName}"`)
+		.join(", ");
+	await db.query(`TRUNCATE ${tableNames} RESTART IDENTITY CASCADE;`);
+}
+
+export default db;
