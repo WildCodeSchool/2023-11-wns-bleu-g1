7587c771ce95ddd7edda09f812d3e5cc7881b0ad
Pouish85 <pouish.iphone@gmail.com>
2024-07-05 10:25:44 +0200

    fix: try to fix db backend access

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import type { AppProps } from "next/app";
 
 const client = new ApolloClient({
-	uri: "http://localhost:4000/",
+	uri: "http://localhost:4000/graphql",
 	cache: new InMemoryCache({
 		addTypename: false,
 	}),
