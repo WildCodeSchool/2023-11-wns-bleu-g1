46f6c20dd9113ce96390a2a9f77bb942e52cac81
Thomas <thomasmarchand63@gmail.com>
2024-07-05 15:07:35 +0200

    :sparkles: prepare for staging

---
+++ fetch_and_deploy_staging.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# fetch_and_deploy_staging
+docker compose -f docker-compose.staging.yml down && \
+docker compose -f docker-compose.staging.yml pull && \
+docker compose -f docker-compose.staging.yml --env-file .env.staging up -d --build
