061244b5f037541d269b59fc0dfeeb6f6a98c766
Ghost <vincent.aipar@gmail.com>
2024-07-11 14:06:15 +0400

    add config for pagination on publics projects query

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,10 +4,45 @@ import "@/styles/globals.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import type { AppProps } from "next/app";
 
-
 const client = new ApolloClient({
 	uri: process.env.NEXT_PUBLIC_GRAPHQL_API_URL || "/graphql",
 	cache: new InMemoryCache({
+		typePolicies: {
+			Query: {
+				fields: {
+					getPublicsProjects: {
+						read(
+							existing,
+							{
+								args,
+							}: {
+								args: { offset?: number; limit?: number } | null;
+							}
+						) {
+							const { offset = 0, limit = existing.length } = args || {};
+							return existing && existing.slice(offset, offset + limit);
+						},
+						keyArgs: [],
+						merge(
+							existing,
+							incoming,
+							{
+								args,
+							}: {
+								args: { offset?: number } | null;
+							}
+						) {
+							const { offset = 0 } = args || {};
+							const merged = existing ? existing.slice(0) : [];
+							for (let i = 0; i < incoming.length; ++i) {
+								merged[offset + i] = incoming[i];
+							}
+							return merged;
+						},
+					},
+				},
+			},
+		},
 		addTypename: false,
 	}),
 	credentials: "include",
