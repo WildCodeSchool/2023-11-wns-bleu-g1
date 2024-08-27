242181217f4a8a38beebdb5a01e0941bae36737d
Thomas <thomasmarchand63@gmail.com>
2024-08-27 17:38:20 +0200

    :sparkles: added ProjectService

--- backend/src/resolvers/project.resolver.ts
+++ backend/src/resolvers/project.resolver.ts
@@ -1,17 +1,19 @@
 import { Arg, Authorized, Ctx, Mutation, Query } from "type-graphql";
+import { GraphQLError } from "graphql";
 
 import Project, { NewProjectInput } from "../entities/project";
 import { Context } from "../interfaces/auth";
-import { GraphQLError } from "graphql";
 import ProjectService from "../services/projet.service";
 import { UserRole } from "../entities/user";
 
 export default class ProjectResolver {
-	@Authorized([UserRole.ADMIN])
-	@Query(() => [Project])
+	constructor(private projectService: ProjectService) {}
+
 	@Authorized([UserRole.VISITOR, UserRole.ADMIN])
+	@Query(() => [Project])
 	async getProjects() {
-		const projects = await ProjectService.getAllProjects();
+		const projectService = new ProjectService();
+		const projects = await projectService.getAll();
 
 		return projects;
 	}
@@ -19,17 +21,19 @@ export default class ProjectResolver {
 	@Authorized([UserRole.VISITOR, UserRole.ADMIN])
 	@Query(() => [Project])
 	async getMyProjects(@Ctx() { currentUser }: Context) {
-		if (!currentUser) throw new GraphQLError("you need to be logged in!");
-
-		const projects = await ProjectService.getAllProjects(currentUser);
+		const projectService = new ProjectService();
+		const projects = await projectService.getAll(currentUser);
 
 		return projects;
 	}
 
 	@Authorized([UserRole.VISITOR, UserRole.ADMIN])
-	@Query(() => [Project])
+	@Query(() => Project)
 	async getProject(@Arg("id") id: string) {
-		return this.projectRepository.findOneOrFail({ where: { id } });
+		const projectService = new ProjectService();
+		const project = await projectService.get(id);
+
+		return project;
 	}
 
 	@Authorized([UserRole.VISITOR, UserRole.ADMIN])
@@ -40,7 +44,8 @@ export default class ProjectResolver {
 	) {
 		if (!currentUser) throw new GraphQLError("you need to be logged in!");
 
-		const project = await ProjectService.create(data, currentUser);
+		const projectService = new ProjectService();
+		const project = await projectService.create(data, currentUser);
 
 		return project;
 	}
--- backend/src/services/projet.service.ts
+++ backend/src/services/projet.service.ts
@@ -1,10 +1,13 @@
 import Project, { NewProjectInput } from "../entities/project";
 import User from "../entities/user";
+import DataSource from "../db";
 
 export default class ProjectService {
-	static getAllProjects = async (user?: User) => {
+	private projectRepository = DataSource.getRepository(Project);
+
+	getAll = async (user?: User) => {
 		// SELECT * FROM Project WHERE user=user;
-		const projects = await Project.find({
+		const projects = await this.projectRepository.find({
 			where: { user },
 			relations: { codes: true, user: true },
 		});
@@ -12,8 +15,14 @@ export default class ProjectService {
 		return projects;
 	};
 
-	static create = async (data: NewProjectInput, user: User) => {
-		const project = Project.create({
+	get = async (id: string) => {
+		const project = await this.projectRepository.findOneByOrFail({ id });
+
+		return project;
+	};
+
+	create = async (data: NewProjectInput, user: User) => {
+		const project = this.projectRepository.create({
 			...data,
 			isPublic: data.isPublic || false,
 			user: user,
