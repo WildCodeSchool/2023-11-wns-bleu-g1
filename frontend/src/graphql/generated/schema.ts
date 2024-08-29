cd5284c22756a5f6da686e1313ab8580508ac01e
Thomas <thomasmarchand63@gmail.com>
2024-08-29 16:27:14 +0200

    :sparkles: added searchbar on profil page

--- frontend/src/graphql/generated/schema.ts
+++ frontend/src/graphql/generated/schema.ts
@@ -256,6 +256,7 @@ export type CreateProjectMutation = { __typename?: 'Mutation', createProject: { 
 export type GetMyProjectsQueryVariables = Exact<{
   limit: Scalars['Float'];
   offset: Scalars['Float'];
+  searchProject: Scalars['String'];
 }>;
 
 
@@ -595,8 +596,8 @@ export type CreateProjectMutationHookResult = ReturnType<typeof useCreateProject
 export type CreateProjectMutationResult = Apollo.MutationResult<CreateProjectMutation>;
 export type CreateProjectMutationOptions = Apollo.BaseMutationOptions<CreateProjectMutation, CreateProjectMutationVariables>;
 export const GetMyProjectsDocument = gql`
-    query GetMyProjects($limit: Float!, $offset: Float!) {
-  getMyProjects(limit: $limit, offset: $offset) {
+    query GetMyProjects($limit: Float!, $offset: Float!, $searchProject: String!) {
+  getMyProjects(limit: $limit, offset: $offset, searchProject: $searchProject) {
     projects {
       id
       isPublic
@@ -625,6 +626,7 @@ export const GetMyProjectsDocument = gql`
  *   variables: {
  *      limit: // value for 'limit'
  *      offset: // value for 'offset'
+ *      searchProject: // value for 'searchProject'
  *   },
  * });
  */
--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,18 +1,45 @@
 import CustomPagination from "@/components/custom-pagination";
 import AuthLayout from "@/components/elements/auth-layout";
-import NotFoundAlert from "@/components/elements/not-found-alert";
 import PageLoader from "@/components/elements/page-loader";
-import ProjectCard from "@/components/elements/project-card";
 import ProjectsContainer from "@/components/elements/ProjectsContainer";
 import UserHeadCard from "@/components/elements/user-head-card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
 	useGetMyProjectsQuery,
 	useGetUserProfileQuery,
 } from "@/graphql/generated/schema";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const ProfilPage = () => {
 	const [page, setPage] = useState(0);
+
+	useEffect(() => {
+		console.log("re render");
+	});
+
+	// states for searchbar
+	const [searchbar, setSearchbar] = useState("");
+	const [selectOption, setSelectOption] = useState("project");
+	const [searchProject, setSearchProject] = useState("");
+
+	const sendSearch = useCallback(
+		(value: string) => {
+			switch (selectOption) {
+				case "project":
+					setSearchProject(value);
+					setSearchbar("");
+					break;
+
+				default:
+					setSearchProject("");
+					setSelectOption("project");
+					setSearchbar("");
+			}
+		},
+		[selectOption]
+	);
+
 	const limit = 12;
 	const offset = page * limit;
 
@@ -21,6 +48,7 @@ const ProfilPage = () => {
 		variables: {
 			limit,
 			offset,
+			searchProject,
 		},
 		notifyOnNetworkStatusChange: true,
 	});
@@ -41,6 +69,34 @@ const ProfilPage = () => {
 			<div className="py-8 space-y-8">
 				<UserHeadCard profile={profile} />
 				<h3 className="text-2xl font-semibold">Mes Projets</h3>
+
+				<div className="flex justify-evenly items-center gap-8">
+					<Input
+						className="bg-white h-[30px] w-[300px] rounded-xl text-black"
+						value={searchbar}
+						onChange={(e) => setSearchbar(e.currentTarget.value)}
+					/>
+					<div className="flex gap-5">
+						<Button
+							className="h-[25px]"
+							onClick={() => sendSearch(searchbar)}
+							disabled={!searchbar.length}
+						>
+							Rechercher
+						</Button>
+
+						<Button
+							className="h-[25px]"
+							variant={"destructive"}
+							onClick={() => {
+								sendSearch("");
+								setSelectOption("project");
+							}}
+						>
+							Reset
+						</Button>
+					</div>
+				</div>
 
 				<ProjectsContainer projects={projects} page={page} isPublic />
 
