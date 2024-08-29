8094a4c3b80843818b01a5d7f7c011ad7341a3b4
Pouish85 <pouish.iphone@gmail.com>
2024-08-29 10:20:32 +0200

    feat: add confirmation popup for delete account

---
+++ frontend/src/components/elements/ConfirmPopUp.tsx
@@ -0,0 +1,80 @@
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+import {useDeleteUserMutation, useGetUserProfileQuery} from "@/graphql/generated/schema";
+import {BadgeCheck} from "lucide-react";
+import {ApolloError} from "@apollo/client";
+import {useToast} from "@/components/ui/use-toast";
+import {useState} from "react";
+import {useRouter} from "next/router";
+
+export function ConfirmPopUp() {
+    const getUserProfileQuery = useGetUserProfileQuery();
+    const router = useRouter();
+    const defaultErrorMessage =
+		"Une erreur est survenue lors de la suppression. Veuillez réessayer.";
+    const [errorMessage, setErrorMessage] = useState<string>(defaultErrorMessage);
+    const { toast } = useToast();
+
+    const [deleteUserMutation, deleteUserResult] = useDeleteUserMutation({
+		onCompleted: () => {
+			toast({
+				icon: <BadgeCheck className="h-5 w-5" />,
+				title: "Utilisateur supprimé",
+				className: "text-success",
+			});
+            console.log(deleteUserResult)
+			router.push("/auth/inscription");
+
+
+		},
+		onError: (err: ApolloError) => {
+			console.error(err);
+			if (err.message.includes("not register")) {
+				setErrorMessage("Aucun n'est lié à cette adresse email.");
+				return;
+			}
+			if (err.message.includes("invalid password")) {
+				setErrorMessage("Les identifiants sont incorrects.");
+				return;
+			}
+			setErrorMessage(defaultErrorMessage);
+            toast({
+                title: errorMessage,
+                className: "text-error",
+            })
+		},
+	});
+
+    const profile = getUserProfileQuery?.data?.getUserProfile || null;
+    function deleteAccount() {
+		console.log("button clicked");
+		if (!profile) return;
+		deleteUserMutation({
+			variables: {
+				deleteUserId: profile.id,
+			},
+		});
+	}
+
+  return (
+      <>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={deleteAccount} >Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </>
+  )
+}
--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -4,36 +4,27 @@ import NotFoundAlert from "@/components/elements/not-found-alert";
 import PageLoader from "@/components/elements/page-loader";
 import ProjectCard from "@/components/elements/project-card";
 import UserHeadCard from "@/components/elements/user-head-card";
-import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
 import {Button, buttonVariants} from "@/components/ui/button";
 import {
 	Card,
 	CardContent,
-	CardDescription,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
 import {
-	useDeleteUserMutation,
 	useGetMyProjectsQuery,
 	useGetUserProfileQuery,
 } from "@/graphql/generated/schema";
-// import { useState } from "react";
-import {BadgeCheck, MessageCircleWarning} from "lucide-react";
 import Link from "next/link";
-import {useRouter} from "next/router";
-import {useToast} from "@/components/ui/use-toast";
-import {ApolloError} from "@apollo/client";
 import {useState} from "react";
+import {AlertDialog, AlertDialogContent, AlertDialogTrigger} from "@/components/ui/alert-dialog";
+import {ConfirmPopUp} from "@/components/elements/ConfirmPopUp";
 
 const ProfilPage = () => {
 	const [page, setPage] = useState(0);
 	const limit = 12;
 	const offset = page * limit;
 
-	const defaultErrorMessage =
-		"Une erreur est survenue lors de la suppression. Veuillez réessayer.";
-	const router = useRouter();
 	const getUserProfileQuery = useGetUserProfileQuery();
 	const getMyProjectsQuery = useGetMyProjectsQuery({
 		variables: {
@@ -42,33 +33,6 @@ const ProfilPage = () => {
 		},
 		notifyOnNetworkStatusChange: true,
 	});
-	const [errorMessage, setErrorMessage] = useState<string>(defaultErrorMessage);
-	const { toast } = useToast();
-	const [deleteUserMutation, deleteUserResult] = useDeleteUserMutation({
-		onCompleted: (data) => {
-			toast({
-				icon: <BadgeCheck className="h-5 w-5" />,
-				title: "Utilisateur supprimé",
-				className: "text-success",
-			});
-			router.push("/auth/inscription");
-
-		},
-		onError: (err: ApolloError) => {
-			console.error(err);
-			if (err.message.includes("not register")) {
-				setErrorMessage("Aucun n'est lié à cette adresse email.");
-				return;
-			}
-			if (err.message.includes("invalid password")) {
-				setErrorMessage("Les identifiants sont incorrects.");
-				return;
-			}
-			setErrorMessage(defaultErrorMessage);
-		},
-	});
-
-	// console.log("getUserProfileQuery: ", getUserProfileQuery);
 	if (getUserProfileQuery.loading || getMyProjectsQuery.loading)
 		return <PageLoader />;
 
@@ -80,16 +44,6 @@ const ProfilPage = () => {
 
 	const projects = data?.projects || [];
 
-	function deleteAccount() {
-		console.log("button clicked");
-		if (!profile) return;
-		deleteUserMutation({
-			variables: {
-				deleteUserId: profile.id,
-			},
-		});
-	}
-
 	return (
 		<AuthLayout>
 			<div className="py-8 space-y-8">
@@ -99,7 +53,7 @@ const ProfilPage = () => {
 					<div>
 						<div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
 							{projects.map((project) => (
-								<ProjectCard key={project.id} project={project} onProfilePage />
+								<ProjectCard key={project.id} project={project} onProfilePage={true}/>
 							))}
 						</div>
 					</div>
@@ -131,14 +85,21 @@ const ProfilPage = () => {
 					</CardHeader>
 					<CardContent className="flex flex-col space-y-3">
 						<Link href="#" className={buttonVariants({ variant: "secondary2"})}>
-							Modifier mon nom d'utilisateur
+							Modifier mon nom de profil
 						</Link>
 						<Link href="#" className={buttonVariants({ variant: "secondary2"})}>
 							Modifier mon mot de passe
 						</Link>
-						<Button className={buttonVariants({ variant: "destructive"})} onClick={deleteAccount}>
-							Supprimer mon compte
-						</Button>
+						<AlertDialog>
+							<AlertDialogTrigger asChild>
+							<Button className={buttonVariants({ variant: "destructive"})}>
+								Supprimer mon compte
+							</Button>
+						</AlertDialogTrigger>
+						<AlertDialogContent>
+							<ConfirmPopUp />
+						</AlertDialogContent>
+						</AlertDialog>
 					</CardContent>
 				</Card>
 			</div>
