1352963c5de90619331293c4f91146a87e580dde
Pouish85 <pouish.iphone@gmail.com>
2024-10-02 13:09:05 +0200

    feat: add cypress to app

--- frontend/src/components/elements/Topbar.tsx
+++ frontend/src/components/elements/Topbar.tsx
@@ -15,6 +15,7 @@ const Topbar = ({ isTopOfPage }: TopbarProps) => {
   return (
     <div
       className={`${topbarBackground} fixed top-0 h-fit w-full flex items-center py-4 z-50`}
+      id="topbar"
     >
       <div className="container mx-auto flex items-center justify-between align-middle transition duration-700 ease-in-out">
         <Link href={"/"}>
