78f38ec79192d67047b747241e62eb419ba65ef0
Pouish85 <pouish.iphone@gmail.com>
2024-10-03 11:55:24 +0200

    feat: test env

--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -1,8 +1,21 @@
-import { ASTNode, graphql, GraphQLSchema, print } from "graphql";
+import {ASTNode, graphql, GraphQLSchema, print} from "graphql";
 
-import db, { clearDb } from "./src/db";
+import db, {clearDb} from "./src/db";
 import getSchema from "./src/schema";
-import { Maybe } from "type-graphql";
+import {Maybe} from "type-graphql";
+
+const mockedEnv = {
+	DB_HOST: "localhost",
+	DB_PORT: "5432",
+	DB_USER: "testuser",
+	DB_PASS: "testpass",
+	DB_NAME: "testdb",
+	CORS_ALLOWED_ORIGINS: "http://localhost:3000",
+	SERVER_PORT: "4000",
+	JWT_PRIVATE_KEY: "testkey",
+};
+
+process.env = {...process.env, ...mockedEnv};
 
 export let schema: GraphQLSchema;
 
@@ -25,16 +38,6 @@ export async function execute(
 		contextValue,
 	});
 }
-
-process.env.DB_HOST = "localhost";
-process.env.DB_PORT = "5432";
-process.env.DB_USER = "testuser";
-process.env.DB_PASS = "testpass";
-process.env.DB_NAME = "testdb";
-process.env.CORS_ALLOWED_ORIGINS = "http://localhost:3000";
-process.env.SERVER_PORT = "4000";
-process.env.JWT_PRIVATE_KEY = "testkey";
-
 beforeAll(async () => {
 	await db.initialize();
 	schema = await getSchema;
