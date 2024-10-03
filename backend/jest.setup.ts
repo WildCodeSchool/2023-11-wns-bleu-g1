57c41e486aa8c81c17148e4eded9d5b95aea96d4
Pouish85 <pouish.iphone@gmail.com>
2024-10-03 11:07:26 +0200

    fix: prettier

--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -26,14 +26,14 @@ export async function execute(
 	});
 }
 
-process.env.DB_HOST = 'localhost';
-process.env.DB_PORT = '5432';
-process.env.DB_USER = 'testuser';
-process.env.DB_PASS = 'testpass';
-process.env.DB_NAME = 'testdb';
-process.env.CORS_ALLOWED_ORIGINS = 'http://localhost:3000';
-process.env.SERVER_PORT = '4000';
-process.env.JWT_PRIVATE_KEY = 'testkey';
+process.env.DB_HOST = "localhost";
+process.env.DB_PORT = "5432";
+process.env.DB_USER = "testuser";
+process.env.DB_PASS = "testpass";
+process.env.DB_NAME = "testdb";
+process.env.CORS_ALLOWED_ORIGINS = "http://localhost:3000";
+process.env.SERVER_PORT = "4000";
+process.env.JWT_PRIVATE_KEY = "testkey";
 
 beforeAll(async () => {
 	await db.initialize();
