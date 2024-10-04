65b4c7b0eb76de8b169bb41b33829ec32db054d8
Pouish85 <pouish.iphone@gmail.com>
2024-10-04 20:55:23 +0200

    feat: project part skeleton

--- frontend/src/components/elements/admin/AdminProjects.tsx
+++ frontend/src/components/elements/admin/AdminProjects.tsx
@@ -1,7 +1,36 @@
+import { useGetProjectsQuery } from "@/graphql/generated/schema";
+import { Separator } from "@radix-ui/react-menu";
+import { Button, buttonVariants } from "@/components/ui/button";
+
 const AdminProjects = () => {
+	const { data } = useGetProjectsQuery();
+	const projects = data?.getProjects || [];
+	console.log("projects: ", projects);
+
 	return (
 		<>
-			<h1>Projets</h1>
+			<h1 className="flex w-full justify-center text-2xl font-bold">
+				Administration des projets
+			</h1>
+			<div className="pt-2 w-full flex justify-center mt-5">
+				<div
+					className="flex flex-col justify-center w-1/3 border border-white rounded-xl px-3 pb-4 pt-2"
+					id="languagesListSection"
+				>
+					<h2>Liste des projets:</h2>
+					<Separator />
+					{projects.map((project) => (
+						<div key={project.id} className="flex justify-between py-1">
+							<p className="flex w-full items-center text-center">
+								{project.title}
+							</p>
+							<div className="flex gap-2">
+								<Button variant="destructive">Supprimer</Button>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 		</>
 	);
 };
--- frontend/src/graphql/generated/schema.ts
+++ frontend/src/graphql/generated/schema.ts
@@ -421,6 +421,20 @@ export type ToggleProjectPublicStateMutation = {
 	};
 };
 
+export type GetProjectsQueryVariables = Exact<{ [key: string]: never }>;
+
+export type GetProjectsQuery = {
+	__typename?: "Query";
+	getProjects: Array<{
+		__typename?: "Project";
+		id: string;
+		title: string;
+		isPublic: boolean;
+		createdAt: any;
+		updatedAt: any;
+	}>;
+};
+
 export type UsersQueryVariables = Exact<{ [key: string]: never }>;
 
 export type UsersQuery = {
@@ -1236,6 +1250,65 @@ export type ToggleProjectPublicStateMutationOptions =
 		ToggleProjectPublicStateMutation,
 		ToggleProjectPublicStateMutationVariables
 	>;
+export const GetProjectsDocument = gql`
+	query GetProjects {
+		getProjects {
+			id
+			title
+			isPublic
+			createdAt
+			updatedAt
+		}
+	}
+`;
+
+/**
+ * __useGetProjectsQuery__
+ *
+ * To run a query within a React component, call `useGetProjectsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetProjectsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetProjectsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetProjectsQuery(
+	baseOptions?: Apollo.QueryHookOptions<
+		GetProjectsQuery,
+		GetProjectsQueryVariables
+	>
+) {
+	const options = { ...defaultOptions, ...baseOptions };
+	return Apollo.useQuery<GetProjectsQuery, GetProjectsQueryVariables>(
+		GetProjectsDocument,
+		options
+	);
+}
+export function useGetProjectsLazyQuery(
+	baseOptions?: Apollo.LazyQueryHookOptions<
+		GetProjectsQuery,
+		GetProjectsQueryVariables
+	>
+) {
+	const options = { ...defaultOptions, ...baseOptions };
+	return Apollo.useLazyQuery<GetProjectsQuery, GetProjectsQueryVariables>(
+		GetProjectsDocument,
+		options
+	);
+}
+export type GetProjectsQueryHookResult = ReturnType<typeof useGetProjectsQuery>;
+export type GetProjectsLazyQueryHookResult = ReturnType<
+	typeof useGetProjectsLazyQuery
+>;
+export type GetProjectsQueryResult = Apollo.QueryResult<
+	GetProjectsQuery,
+	GetProjectsQueryVariables
+>;
 export const UsersDocument = gql`
 	query Users {
 		users {
