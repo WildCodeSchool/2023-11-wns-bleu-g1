1ca2b65d3f0651aade58a85b96ec316af1b8075d
Thomas Marchand <115934120+ThomMarchand@users.noreply.github.com>
2024-11-03 10:58:00 +0100

    ✨  refacto searchbar logic (#222)
    * :sparkles: refacto searchbar logic
    
    * :sparkles: returns

---
+++ frontend/src/components/elements/searchbar/Searchbar.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+interface Props {
+	setSearchbar: React.Dispatch<React.SetStateAction<string>>;
+	setSelectOption: React.Dispatch<React.SetStateAction<string>>;
+	sendSearch: (value: string) => void;
+	searchbar: string;
+}
+
+const Searchbar = ({
+	setSearchbar,
+	setSelectOption,
+	sendSearch,
+	searchbar,
+}: Props) => {
+	return (
+		<div className="flex justify-evenly items-center gap-8">
+			<Input
+				className="bg-white h-[30px] w-[300px] rounded-xl text-black"
+				value={searchbar}
+				onChange={(e) => setSearchbar(e.currentTarget.value)}
+				onKeyDown={(e) => {
+					e.key === "Enter" && sendSearch(searchbar);
+					if (e.key === "Escape") {
+						sendSearch("");
+						setSearchbar("");
+					}
+				}}
+			/>
+
+			<div className="flex gap-5">
+				<Button
+					className="h-[25px]"
+					onClick={() => sendSearch(searchbar)}
+					disabled={!searchbar.length}
+				>
+					Rechercher
+				</Button>
+
+				<Button
+					className="h-[25px]"
+					variant={"dark"}
+					onClick={() => {
+						sendSearch("");
+						setSelectOption("project");
+					}}
+				>
+					Reset
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default Searchbar;
